97741d189926ef8df2f2a4e26a8e97082a014498
Rolukas <rolandosumoza@gmail.com>
2024-04-01 19:33:17 -0600

    environment variables

---
+++ concepts/environment.js
@@ -0,0 +1,7 @@
+/* let hi = "Hola";
+console.log(hi); */
+
+// Execute this like
+// Name=Rolando node concepts/environment.js
+let name = process.env.Name || "No Name";
+console.log("Hola " + name);
