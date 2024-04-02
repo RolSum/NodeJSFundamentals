3496edb33ca5fe6ba3ed099a5b013d549350d4a5
Rolukas <rolandosumoza@gmail.com>
2024-04-01 19:41:26 -0600

    Nodemon and PM2

--- concepts/environment.js
+++ concepts/environment.js
@@ -5,3 +5,10 @@ console.log(hi); */
 // Name=Rolando node concepts/environment.js
 let name = process.env.Name || "No Name";
 console.log("Hola " + name);
+
+// Nodemon, is a npm package to keep the server alive
+// PM2 is a more advanced alternative.
+
+// Example: nodemon --watch concepts/environment.js
+// Now, we don't need to use nodemon since node has the capaability to keep alive the server without any package.
+// Example: node --watch concepts/environment.js
